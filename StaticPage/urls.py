674cfa36c9f040bc63ba828dae4d1031428ed73b
Lei Feng <leifeng@Leis-iMac.local>
2020-08-07 23:46:35 -0700

    update with CRUD

--- StaticPage/urls.py
+++ StaticPage/urls.py
@@ -3,5 +3,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.HelloDjango.as_view())
+    #path('', views.HelloDjango.as_view())
+    path('', views.PostListView.as_view(), name = "home"),
+    path('post/<int:pk>/', views.PostDetailView.as_view(), name = "post"),
+    path('post/new/', views.PostCreateView.as_view(), name = "make_post"),
+    path('post/edit/<int:pk>', views.PostUpdataView.as_view(), name = "edit"),
+    path('post/delete/<int:pk>', views.PostDeleteView.as_view(), name = "delete")
 ]
--- StaticPage/views.py
+++ StaticPage/views.py
@@ -1,7 +1,33 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView, DetailView, CreateView, UpdateView, DeleteView
+from .models import Post
+from django.urls import reverse_lazy
 
 class HelloDjango(TemplateView):
     template_name = "test.html"
 
 # Create your views here.
+
+class PostListView(ListView):
+    model = Post
+    template_name = "home.html"
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = "post_detail.html"
+
+class PostCreateView(CreateView):
+    model = Post
+    template_name = "post_new.html"
+    fields = "__all__"
+
+class PostUpdataView(UpdateView):
+    model = Post
+    template_name = "post_edit.html"
+    fields = ['title']
+
+class PostDeleteView(DeleteView):
+    model = Post
+    template_name = "post_delete.html"
+    success_url = reverse_lazy('home')
+    
